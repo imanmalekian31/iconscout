97b51271a3148d13afaabff4b465b76207ef2911
iman malekian <imanmalekian31@gmail.com>
2024-06-20 08:50:32 +0330

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  srcDir: './src',
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: ['@nuxt/eslint'],
+});
