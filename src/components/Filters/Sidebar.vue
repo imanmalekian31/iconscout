d328a89459d0aed0ede885bdf7ff99b696a9db4c
iman malekian <imanmalekian31@gmail.com>
2024-06-21 07:47:55 +0330

    fix(sidebar): :bug: fix hydration error

--- src/components/Filters/Sidebar.vue
+++ src/components/Filters/Sidebar.vue
@@ -52,50 +52,52 @@ const filters = [
       class="hidden lg:block w-[260px] border-r border-[#EBEDF5] h-[calc(100vh-130px)] sticky top-[190px] xl:top-[130px]"
     >
       <div v-for="filter in filters" :key="filter.name" class="border-b">
-        <Disclosure defaultOpen v-slot="{ open }">
-          <DisclosureButton
-            class="flex items-center justify-between py-5 px-6 w-full"
-          >
-            {{ filter.title }}
-            <ChevronDownIcon
-              :class="[
-                'size-5',
-                open
-                  ? 'rotate-180 transition-transform ease-linear'
-                  : 'transition-transform ease-linear',
-              ]"
-            />
-          </DisclosureButton>
-          <transition
-            enter-active-class="transition duration-100 ease-out"
-            enter-from-class="transform scale-95 opacity-0"
-            enter-to-class="transform scale-100 opacity-100"
-            leave-active-class="transition duration-75 ease-out"
-            leave-from-class="transform scale-100 opacity-100"
-            leave-to-class="transform scale-95 opacity-0"
-          >
-            <DisclosurePanel>
-              <div
-                v-for="option in filter.options"
-                :key="option.id"
-                class="flex items-center mx-6 mb-3"
-              >
-                <input
-                  :id="option.id"
-                  name="asset-type"
-                  type="radio"
-                  class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                />
-                <label
-                  :for="option.id"
-                  class="ml-3 block text-sm font-medium leading-6 text-gray-900 cursor-pointer"
+        <ClientOnly>
+          <Disclosure defaultOpen v-slot="{ open }">
+            <DisclosureButton
+              class="flex items-center justify-between py-5 px-6 w-full"
+            >
+              {{ filter.title }}
+              <ChevronDownIcon
+                :class="[
+                  'size-5',
+                  open
+                    ? 'rotate-180 transition-transform ease-linear'
+                    : 'transition-transform ease-linear',
+                ]"
+              />
+            </DisclosureButton>
+            <transition
+              enter-active-class="transition duration-100 ease-out"
+              enter-from-class="transform scale-95 opacity-0"
+              enter-to-class="transform scale-100 opacity-100"
+              leave-active-class="transition duration-75 ease-out"
+              leave-from-class="transform scale-100 opacity-100"
+              leave-to-class="transform scale-95 opacity-0"
+            >
+              <DisclosurePanel>
+                <div
+                  v-for="option in filter.options"
+                  :key="option.id"
+                  class="flex items-center mx-6 mb-3"
                 >
-                  {{ option.title }}
-                </label>
-              </div>
-            </DisclosurePanel>
-          </transition>
-        </Disclosure>
+                  <input
+                    :id="option.id"
+                    name="asset-type"
+                    type="radio"
+                    class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+                  <label
+                    :for="option.id"
+                    class="ml-3 block text-sm font-medium leading-6 text-gray-900 cursor-pointer"
+                  >
+                    {{ option.title }}
+                  </label>
+                </div>
+              </DisclosurePanel>
+            </transition>
+          </Disclosure>
+        </ClientOnly>
       </div>
     </div>
   </Transition>
