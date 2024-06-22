3e4f52300619117d68f2cd6cf5296d9bbeeebf45
iman malekian <imanmalekian31@gmail.com>
2024-06-22 20:05:01 +0330

    feat(assets): :art: add assets info

---
+++ src/components/assets/AssetsInfo.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const filterStore = useFiltersStore();
+</script>
+
+<template>
+  <div class="py-8 px-6 bg-[#FAFAFC]">
+    <h1 class="text-2xl xl:text-4xl font-bold mb-1">
+      {{ filterStore.totalAssets.toLocaleString() }} Limit
+      {{
+        {
+          all: 'Design Assets',
+          lottie: 'Animations',
+          '3d': '3D Illustrations',
+          illustration: 'Illustrations',
+          icon: 'Icons',
+        }[filterStore.filters.asset]
+      }}
+    </h1>
+    <div class="text-sm text-[#5A607D] truncate">
+      {{ filterStore.totalAssets.toLocaleString() }}
+      {{ ['all', 'icon'].includes(filterStore.filters.asset) ? '2D' : '3D' }}s
+      exclusively selected by our designer community.
+    </div>
+  </div>
+</template>
