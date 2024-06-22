66c26da1d37500bb48aaa56e38fd7a6dcf1d13c1
iman malekian <imanmalekian31@gmail.com>
2024-06-22 20:01:42 +0330

    chore(apis): :art: use v2 instead of v3

--- src/apis/filter.ts
+++ src/apis/filter.ts
@@ -1,5 +1,7 @@
 import type { AssetResponse, FetchAssetsParams, FetchResponse } from '~/types';
 
 export function fetchAssetsListAPI(params: FetchAssetsParams) {
-  return useApiFetch<FetchResponse<AssetResponse>>('/v3/search', { params });
+  return useApiFetch<FetchResponse<AssetResponse>>('/v2/new-search', {
+    params,
+  });
 }
