ac5d0590325ebe0711058ff503fd5a4ca1fabc0e
iman malekian <imanmalekian31@gmail.com>
2024-06-22 20:21:51 +0330

    chore(apis): :art: Minor improvement

--- src/apis/filter.ts
+++ src/apis/filter.ts
@@ -1,7 +1,9 @@
 import type { AssetResponse, FetchAssetsParams, FetchResponse } from '~/types';
 
 export function fetchAssetsListAPI(params: FetchAssetsParams) {
-  return useApiFetch<FetchResponse<AssetResponse>>('/v2/new-search', {
+  const url = params.asset === 'lottie' ? '/v2/new-search' : '/v3/search';
+
+  return useApiFetch<FetchResponse<AssetResponse>>(url, {
     params,
   });
 }
