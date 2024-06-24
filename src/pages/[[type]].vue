eaf588402c346060dbdb90a83aef750a361a2eca
iman malekian <imanmalekian31@gmail.com>
2024-06-24 19:34:49 +0330

    feat(seo): :sparkles: add meta tags

--- src/pages/[[type]].vue
+++ src/pages/[[type]].vue
@@ -1,21 +1,33 @@
 <script setup lang="ts">
-useHead({
-  title: 'Shape Design Assets - Free in SVG, PNG, BLEND, GIF | IconScout',
-  meta: [
-    {
-      name: 'description',
-      content:
-        'Download 304939 high-quality Shape design assets - 3D Illustrations, Lottie Animations, Vector Icons and Illustrations. Available for free or premium at IconScout.',
-    },
-  ],
-});
-
 const filterStore = useFiltersStore();
+
+const titles = {
+  all: 'Design Assets',
+  lottie: 'Animations',
+  '3d': '3D Illustrations',
+  illustration: 'Illustrations',
+  icon: 'Icons',
+};
+
+function pageTitle() {
+  return `${titles[filterStore.filters.asset]} | IconScout`;
+}
+
+function pageDescription() {
+  return `Free Download ${filterStore.assets.count.toLocaleString()} Business Target Vector Icons for commercial and personal use in Canva, Figma, Adobe XD, After Effects, Sketch & more. Available in line, flat, gradient, isometric, glyph, sticker & more design styles.`;
+}
+
+useSeoMeta({
+  title: pageTitle,
+  ogTitle: pageTitle,
+  description: pageDescription,
+  ogDescription: pageDescription,
+  ogImage: 'https://iconscout-iman.vercel.app/images/og.png',
+});
 </script>
 
 <template>
   <AssetsInfo />
-
   <main>
     <FiltersInline />
     <div class="flex">
