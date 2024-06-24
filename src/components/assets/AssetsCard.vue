d5d605f577b70f53b3335b7ddafcb1984c26f425
iman malekian <imanmalekian31@gmail.com>
2024-06-24 22:34:43 +0330

    refactor(assets-card): :recycle: refactor styles with tailwind

---
+++ src/components/assets/AssetsCard.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+import { ArrowDownTrayIcon } from '@heroicons/vue/24/outline';
+import { FolderPlusIcon } from '@heroicons/vue/24/solid';
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue';
+import { Vue3Lottie } from 'vue3-lottie';
+
+import { downloadAssetAPI } from '~/apis/asset';
+import type { Asset } from '~/types';
+
+const layoutStore = useLayoutStore();
+
+interface Props {
+  asset: Asset & { hover: boolean };
+}
+
+defineProps<Props>();
+
+function getSrcSet(urls: Asset['urls']) {
+  return Object.values(urls)
+    .map((value, idx) => `${value} ${idx + 1}x`)
+    .join(', ');
+}
+
+function downloadIcon(uuid: string) {
+  downloadAssetAPI(uuid, 'svg').then((data) => {
+    const url = data.response.download.download_url;
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = url;
+    link.click();
+  });
+}
+</script>
+
+<template>
+  <article
+    :class="[
+      'relative',
+      asset.asset === 'lottie' ? '!bg-white' : '!bg-gray-50',
+      asset.asset === 'icon' ? 'iconArticle' : 'illustrateArticle',
+    ]"
+    :title="asset.name"
+    @mouseenter="asset.hover = true"
+    @mouseleave="asset.hover = false"
+  >
+    <picture v-if="asset.asset !== 'lottie'">
+      <source :srcset="getSrcSet(asset.urls)" type="image/png" />
+      <img :alt="asset.name" loading="lazy" :srcset="getSrcSet(asset.urls)" />
+    </picture>
+
+    <template v-else-if="'original' in asset.urls">
+      <DotLottieVue
+        v-if="layoutStore.toggleDotLottie"
+        style="height: 500px; width: 500px"
+        autoplay
+        loop
+        :src="asset.urls.original"
+      />
+      <ClientOnly v-else>
+        <Vue3Lottie :animationLink="asset.urls.original" />
+      </ClientOnly>
+    </template>
+
+    <template v-if="asset.hover">
+      <button
+        type="button"
+        class="absolute z-10 right-3 top-3 bg-gray-75 border border-gary-100 text-gray-650 p-1.5 rounded-lg hover:bg-gray-100 focus:outline-none"
+      >
+        <FolderPlusIcon class="size-6" aria-hidden="true" />
+      </button>
+
+      <button
+        type="button"
+        class="absolute z-10 right-3 bottom-3 bg-gray-75 border border-gary-100 text-gray-650 p-1.5 rounded-lg hover:bg-gray-100 focus:outline-none"
+        @click="downloadIcon(asset.uuid)"
+      >
+        <ArrowDownTrayIcon class="size-6" aria-hidden="true" />
+      </button>
+
+      <h2
+        :class="[
+          'absolute z-10 left-3 bottom-3 text-xs truncate text-gray-700',
+          ['all', 'icon'].includes(asset.asset) ? 'max-w-16' : 'max-w-44',
+        ]"
+      >
+        {{ asset.name }}
+      </h2>
+    </template>
+  </article>
+</template>
+
+<style>
+.iconArticle {
+  @apply flex items-center justify-center min-w-[120px] md:min-w-[140px] min-h-[120px] md:min-h-[140px] relative;
+  @apply rounded-lg;
+  @apply bg-gray-50;
+  @apply cursor-pointer;
+
+  img {
+    @apply transition-transform duration-200 ease-in-out;
+  }
+
+  &:hover {
+    &::after {
+      @apply absolute top-0 right-0 bottom-0 left-0;
+      @apply rounded-lg;
+      @apply bg-[#2e334c1a];
+      @apply bg-auto bg-no-repeat;
+      @apply content-[''];
+    }
+
+    img {
+      @apply scale-110;
+    }
+  }
+}
+
+.illustrateArticle {
+  @apply flex items-center justify-center h-[13.125rem] relative;
+  @apply py-2 px-3;
+  @apply rounded-lg;
+  @apply bg-gray-50;
+  @apply overflow-hidden;
+  @apply cursor-pointer;
+
+  picture,
+  video {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+
+    img {
+      @apply max-h-full;
+      @apply transition-transform duration-200 ease-in-out;
+    }
+  }
+
+  &:hover {
+    &::after {
+      @apply absolute top-0 right-0 bottom-0 left-0;
+      @apply rounded-lg;
+      @apply bg-[#2e334c1a];
+      @apply bg-auto bg-no-repeat;
+      @apply content-[''];
+    }
+
+    img {
+      @apply scale-110;
+    }
+  }
+}
+</style>
--- src/components/assets/AssetsList.vue
+++ src/components/assets/AssetsList.vue
@@ -1,34 +1,9 @@
 <script setup lang="ts">
-import { ArrowDownTrayIcon } from '@heroicons/vue/24/outline';
-import { FolderPlusIcon } from '@heroicons/vue/24/solid';
-import { DotLottieVue } from '@lottiefiles/dotlottie-vue';
-import { Vue3Lottie } from 'vue3-lottie';
-
-import { downloadAssetAPI } from '~/apis/asset';
-import type { Asset } from '~/types';
-
-const layoutStore = useLayoutStore();
 const filterStore = useFiltersStore();
 const { observer } = useInfiniteScroll(loadMore);
 
 function loadMore() {
   filterStore.fetchNextPage();
-}
-
-function getSrcSet(urls: Asset['urls']) {
-  return Object.values(urls)
-    .map((value, idx) => `${value} ${idx + 1}x`)
-    .join(', ');
-}
-
-function downloadIcon(uuid: string) {
-  downloadAssetAPI(uuid, 'svg').then((data) => {
-    const url = data.response.download.download_url;
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = url;
-    link.click();
-  });
 }
 </script>
 
@@ -45,71 +20,19 @@ function downloadIcon(uuid: string) {
       v-if="!filterStore.assets.data.length"
       v-for="i in 42"
       :key="i"
-      class="animate-pulse !bg-gray-200"
+      :class="[
+        ['icon', 'all'].includes(filterStore.filters.asset)
+          ? 'min-w-[120px] md:min-w-[140px] min-h-[120px] md:min-h-[140px]'
+          : 'h-[13.125rem]',
+        'animate-pulse rounded-lg !bg-gray-200',
+      ]"
     />
 
-    <article
-      v-else
-      v-for="assetItem in filterStore.assets.data"
-      :key="assetItem.id"
-      :class="[
-        'relative',
-        filterStore.filters.asset === 'lottie' ? '!bg-white' : '!bg-gray-50',
-      ]"
-      :title="assetItem.name"
-      @mouseenter="assetItem.hover = true"
-      @mouseleave="assetItem.hover = false"
-    >
-      <picture v-if="filterStore.filters.asset !== 'lottie'">
-        <source :srcset="getSrcSet(assetItem.urls)" type="image/png" />
-        <img
-          :alt="assetItem.name"
-          loading="lazy"
-          :srcset="getSrcSet(assetItem.urls)"
-        />
-      </picture>
-
-      <template v-else-if="'original' in assetItem.urls">
-        <DotLottieVue
-          v-if="layoutStore.toggleDotLottie"
-          style="height: 500px; width: 500px"
-          autoplay
-          loop
-          :src="assetItem.urls.original"
-        />
-        <ClientOnly v-else>
-          <Vue3Lottie :animationLink="assetItem.urls.original" />
-        </ClientOnly>
-      </template>
-
-      <template v-if="assetItem.hover">
-        <button
-          type="button"
-          class="absolute z-10 right-3 top-3 bg-gray-75 border border-gary-100 text-gray-650 p-1.5 rounded-lg hover:bg-gray-100 focus:outline-none"
-        >
-          <FolderPlusIcon class="size-6" aria-hidden="true" />
-        </button>
-
-        <button
-          type="button"
-          class="absolute z-10 right-3 bottom-3 bg-gray-75 border border-gary-100 text-gray-650 p-1.5 rounded-lg hover:bg-gray-100 focus:outline-none"
-          @click="downloadIcon(assetItem.uuid)"
-        >
-          <ArrowDownTrayIcon class="size-6" aria-hidden="true" />
-        </button>
-
-        <h2
-          :class="[
-            'absolute z-10 left-3 bottom-3 text-xs truncate text-gray-700',
-            ['all', 'icon'].includes(filterStore.filters.asset)
-              ? 'max-w-16'
-              : 'max-w-44',
-          ]"
-        >
-          {{ assetItem.name }}
-        </h2>
-      </template>
-    </article>
+    <AssetsCard
+      v-for="asset in filterStore.assets.data"
+      :key="asset.id"
+      :asset="asset"
+    />
   </section>
   <div class="flex items-center justify-center my-10" ref="observer">
     <SvgoSpinner class="text-blue-500 animate-spin text-7xl" />
@@ -122,58 +45,8 @@ function downloadIcon(uuid: string) {
   grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
   gap: 0.25rem;
 
-  article {
-    @apply bg-gray-50;
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-width: 140px;
-    min-height: 140px;
-    border-radius: 0.5rem;
-    cursor: pointer;
-
-    img {
-      transition: transform 200ms ease-in-out;
-    }
-
-    &:hover {
-      &::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        border-radius: 0.5rem;
-        background-color: #2e334c1a;
-        background-size: 100px auto;
-        background-repeat: no-repeat;
-      }
-
-      img {
-        transform: scale(1.1);
-      }
-    }
-  }
-
-  article.animate-pulse {
-    &:hover {
-      &::after,
-      img {
-        transform: none !important;
-        background-color: unset !important;
-      }
-    }
-  }
-
   @media (max-width: 1024px) {
     grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
-
-    article {
-      min-width: 120px;
-      min-height: 120px;
-    }
   }
 }
 
@@ -181,62 +54,6 @@ function downloadIcon(uuid: string) {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
   gap: 0.75rem;
-
-  article {
-    @apply bg-gray-50;
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 13.125rem;
-    padding: 0.5rem 0.75rem;
-    border-radius: 0.5rem;
-    overflow: hidden;
-    cursor: pointer;
-
-    picture,
-    video {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-      height: 100%;
-
-      img {
-        max-height: 100%;
-        transition: transform 200ms ease-in-out;
-      }
-    }
-
-    &:hover {
-      &::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        border-radius: 0.5rem;
-        background-color: #2e334c1a;
-        background-size: 100px auto;
-        background-repeat: no-repeat;
-      }
-
-      img {
-        transform: scale(1.1);
-      }
-    }
-  }
-
-  article.animate-pulse {
-    &:hover {
-      &::after,
-      img {
-        transform: none !important;
-        background-color: unset !important;
-      }
-    }
-  }
 
   @media (max-width: 1024px) {
     grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
